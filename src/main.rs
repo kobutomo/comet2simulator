42a05d44c4e5e7047e514ee027c53b55d9a5284e
kobutomo <n.tomoro13@gmail.com>
2021-12-13 20:40:19 +0900

    TUI ライブラリ導入

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,35 @@
-mod memory;
 mod comet;
+mod memory;
 
-fn main() {
-    let mut mem = memory::new();
-    mem.write(12, 7000);
-    let i = mem.read(12).unwrap();
-    println!("{:08}", i)
+use std::io;
+use termion::raw::IntoRawMode;
+use tui::backend::TermionBackend;
+use tui::layout::{Constraint, Direction, Layout};
+use tui::widgets::{Block, Borders, Widget};
+use tui::Terminal;
+
+fn main() -> Result<(), io::Error> {
+    let comet = comet::Comet::new();
+    let stdout = io::stdout().into_raw_mode()?;
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.draw(|f| {
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(1)
+            .constraints(
+                [
+                    Constraint::Percentage(10),
+                    Constraint::Percentage(80),
+                    Constraint::Percentage(10),
+                ]
+                .as_ref(),
+            )
+            .split(f.size());
+        let block = Block::default().title("Block").borders(Borders::ALL);
+        f.render_widget(block, chunks[0]);
+        let block = Block::default().title("Block 2").borders(Borders::ALL);
+        f.render_widget(block, chunks[1]);
+    })?;
+    Ok(())
 }
